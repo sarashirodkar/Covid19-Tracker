1c7dce9b08789cc3a4ca6e44dbbe79caff363ea8
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2021-03-21 14:20:02 +0530

    project start commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { FormControl,Select, MenuItem } from '@material-ui/core';
 import './App.css';
+import InfoBox from './InfoBox';
 
 function App() {
+
+  const [countries,setCountries] = useState([]);
+  const [countryValue, setCountryValue] = useState("Worldwide");
+
+  useEffect(()=>{
+    const getCountries = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+            .then(response => response.json())
+            .then(data => { const country = data.map((data) => ({ name:data.country,
+                                                                  value:data.countryInfo.iso2 }))
+                            setCountries(country)
+                          })
+    }
+    getCountries();
+  },[]);
+
+  const handleCountryCode = (event) => {
+    const countryCode = event.target.value;
+    setCountryValue(countryCode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/** Header */}
+      <div className="app__header">
+        <div className="app__heading"><h1>Covid19 Tracker</h1></div>
+            <FormControl className="app__dropdown">
+              <Select variant="outlined" value={countryValue} onChange={handleCountryCode}>
+                 <MenuItem value="Worldwide">Worldwide</MenuItem>
+                 {countries.map((country,index) =><MenuItem key={index} value={country.value}>{country.name}</MenuItem>)}
+              </Select>
+            </FormControl>
+      </div>
+      <div className="app__stats">
+        {/** InfoBox */}
+        <InfoBox title="CoronaVirusCases" cases={1223} total={100}/>  
+        {/** InfoBox */}
+        <InfoBox title="Recovered" cases={123} total={200}/>  
+        {/** InfoBox */}
+        <InfoBox title="Deaths" cases={1234} total={300}/>
+      </div>
     </div>
   );
 }
