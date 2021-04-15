4ffef2de6cfe2b457e2a46dc7e1b4c1282167d6f
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2021-04-15 22:22:55 +0530

    Map Data and Styling

--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import './Map.css';
+import { mapDataCountries, showDataOnMap } from './util';
 
-function Map() {
+function Map({casesType, countries,center, zoom}) {
+
+    function ChangeView({ center, zoom }) {
+        const map = useMap();
+        map.setView(center, zoom);
+        return null;
+      }
     return (
-        <div className="Map">
-            <h2>I am Map</h2>
+        <div className="map">
+            <MapContainer className="map-container" casesType={casesType} center={center} zoom={zoom} scrollWheelZoom={false}>
+            <ChangeView center={center} zoom={zoom} />
+                <TileLayer 
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
+                {showDataOnMap(countries, casesType)};
+            </MapContainer>
         </div>
     )
 }
