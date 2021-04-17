554b6ee80be3dc792385a8e56f5875e5c396aacd
jeet-mahesh-shirodkar <shirodkar.jeet.15ee5062@gmail.com>
2021-04-17 11:38:49 +0530

    active bar

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {Card,CardContent, Typography} from "@material-ui/core";
+import './InfoBox.css';
+import { prettyPrintStat } from './util';
 
-function InfoBox({title,cases,total}) {
+function InfoBox({title,cases,total,active,...props}) {
     return (
-        <Card className="infoBox">
+        <Card className={`infoBox ${active && props.isCases && "infoBox--isCases"} 
+                                  ${active && props.isRecovered && "infoBox--isRecovered"}
+                                  ${active && props.isDeaths && "infoBox--isDeaths"}`} onClick={props.onClick}>
             <CardContent>
                 <Typography className="infoBox__title" color="textSecondary">{title}</Typography>
-                <h2 className="infoBox__cases">{cases}</h2>
-                <Typography className="infoBox__total" color="textSecondary">{total} Total</Typography>
+                <h2 className={`infoBox__cases ${props.isCases && "infoBox--isCasesColor"} 
+                                  ${props.isRecovered && "infoBox--isRecoveredColor"}
+                                  ${props.isDeaths && "infoBox--isDeathsColor"}`}>{prettyPrintStat(cases)}</h2>
+                <Typography className="infoBox__total" color="textSecondary">{prettyPrintStat(total)}Total</Typography>
             </CardContent>
         </Card>
     )
